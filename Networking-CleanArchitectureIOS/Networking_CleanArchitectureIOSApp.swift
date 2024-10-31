8d47dda77b2daecefba0d5f7a1251e75d1e7aca6
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-10-31 10:38:53 +0700

    Initial Commit

---
+++ Networking-CleanArchitectureIOS/Networking_CleanArchitectureIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_CleanArchitectureIOSApp.swift
+//  Networking-CleanArchitectureIOS
+//
+//  Created by Đoàn Văn Khoan on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Networking_CleanArchitectureIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
